4104970e1eb4e5ea813f6ca6a95735149d5f3ca3
olivier rousset <orousset@hotmail.com>
2017-11-19 19:22:05 +0800

    retry

---
+++ primeCalculator/primeCalculator/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primeCalculator
+{
+    public class Prime
+    {
+        bool isPrime(int input) {
+            bool result = false;
+            for (int cpt = 1; cpt < Math.Sqrt(input); cpt++) {
+                if (Math.IEEERemainder(input, cpt) == 0) { result = true; }
+            }
+            return result;
+        }
+    }
+}
